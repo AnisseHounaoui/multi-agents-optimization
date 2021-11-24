70ed06a7d494b37fcbcf6890ef053abea606fc56
houa2807 <houa2807@gegi-gsi-w003.gegi.usherbrooke.ca>
2021-11-24 09:09:33 -0500

    add comments

--- rdv_statique.m
+++ rdv_statique.m
@@ -1,17 +1,17 @@
-n = 15; %nb des noeuds
-d = 40; %nb des itérations
-c = 1/(n+1);
-a = 200; %longueur de l'espace
-b = 150; %largeur de l'espace
+n = 15; %nombre des noeuds
+d = 40; %nombre d'itérations
+c = 1/(n+1); %constante
+a = 200; %longueur du plan
+b = 150; %largeur du plan
 r_det = (((a.^2)+(b.^2)).^(1/2))/3; %rayon de détection
 r_sec = r_det/3; %rayon de sécurité
 
-x1 = randsample(double(0:5:a),n); %double(0:5:a) to generate a list of values from 0 to a seperated by 5
-                                 %randsample is for taking values randomly
-                                 %from 0 to n with 5 seperateds
-y1 = randsample(double(0:5:b),n);
-%dist = ((x1(1)-x1(5)).^2 + (y1(1)-y1(5)).^2).^(1/2);
-M = positionToAdjMatrix(x1,y1,r_det);
+x_agent = randsample(double(0:5:a),n); %double(0:5:a) pour generer une liste de valeur de "0" à "a" avec un espace de 5 entre les valeurs
+                                 %randsample pour choisir "n" valeurs 
+                                 %de 0 a n avec 5 séparés
+y_agent = randsample(double(0:5:b),n);
+
+M = positionToAdjMatrix(x_agent,y_agent,r_det);
 
 %M = reshape(M,[n,n]);
 P = pMatrix(M,c,n);
@@ -24,21 +24,25 @@ C = adjacentToCom(M,n)
 
 A = matCom(M,W);
 
-%show_nodes(M,W,x1,y1,d,n)
-show_nodes_sec(C,x1,y1,r_sec,d,n)
+%show_nodes(M,W,x_agent,y_agent,d,n) %en respectant la distence de
+%detection entre agents
+show_nodes_sec(C,x_agent,y_agent,r_sec,d,n)%affichage du graphe en implementant
 
 %show_graph(M,n,d)
 
-function mat = positionToAdjMatrix(x,y,r_det)
-    n = length(x);
-    mat = zeros(n);
+function mat = positionToAdjMatrix(x,y,r_det) %matrice d'adjacence qui contient :
+                                              % "1" sur (i,j) et (j,i) si
+                                              % l'agent i est proche d'une distance
+                                              % inferieur au rayonde detection de l'agent j
+    n = length(x); % nombre d'agent
+    mat = zeros(n); %matrice de zero
     
     for i=1:n
         for j=1:n
             
-            dist_ij = ((x(i)-x(j)).^2 + (y(i)-y(j)).^2).^(1/2);
+            dist_ij = ((x(i)-x(j)).^2 + (y(i)-y(j)).^2).^(1/2); %calcul de distence entre les agent
             if dist_ij <= r_det
-                mat(i,j) = 1;
+                mat(i,j) = 1; %si distance entre les deux agents est inférieur au rayon de sécurité ajouter 1 en (i,j) et (j,i)
             end
         end
     end
@@ -46,25 +50,25 @@ function mat = positionToAdjMatrix(x,y,r_det)
     
     
     for i=1:n
-        mat(i,i) = 0;
+        mat(i,i) = 0;%0 sinon
     end
 
     
 end
 
-function L = Laplacienne(M,n)
-    D = eye(length(M));
+function Laplacienne = Laplacienne(M,n)
+    D = eye(length(M)); %Matrice d'identité
     deg = sum(M,2);
     for i=1:n
-       D(i,i) = deg(i);
+       D(i,i) = deg(i); %matrice de degrée (chaque (i,j) contient le nombre de liens entre le neoud i et j)
     end
     
-    L = D - M;
+    Laplacienne = D - M; %matrice laplacienne est la difference de la matrice de degree et la matrice d'adjacence
 end
 
-function C = adjacentToCom(M,n)
-    C(1:n,1:n) = M;
-    fact = sum(M,2);
+function C = adjacentToCom(M,n) %extraire la matrice de communication à partir de la matrice d'adjacence
+    C(1:n,1:n) = M; %faire une copie de la amtrice M
+    fact = sum(M,2); 
     %M = double(M);
     for i=1:n
         if fact(i) == 0
@@ -82,24 +86,24 @@ end
 
 function m = randStocastic(n)
     matrix = rand (n,n)
-    sumo  = sum(matrix,2);
+    somme_facteur  = sum(matrix,2);
     for i=1:n
-        matrix(i,:) = matrix(i,:)/sumo(i,:);
+        matrix(i,:) = matrix(i,:)/somme_facteur(i,:);
     end
     m = matrix;
 end
 
-function A = matCom(M,W)
+function A = matCom(M,W) %matrice de communication à partir de la amatrice d'adjacence et la matrice de priorité
 
-    I = eye(length(M));
-    J = ones(length(M));
+    I = eye(length(M)); %matrice d'identite
+    J = ones(length(M));%matrice qui contient des "1" partout
     Q = M + I;
     Qbar = J - Q;
     %C = dot(W.*Qbar,J);
-    A = Q.*W + ((W.*Qbar)*J).*I;
+    A = Q.*W + ((W.*Qbar)*J).*I; %matrice de communication
 end
 
-function [x,y]  = mouvement(x,y,A,n)
+function [x,y]  = mouvement(x,y,A,n) %couple de mouvements d'un agent
     for i=1:n 
         for j=1:n
             x(i)= x(i)+(x(j)-x(i))*A(i,j);
@@ -108,7 +112,7 @@ function [x,y]  = mouvement(x,y,A,n)
     end
 end
 
-function [x,y] = mouvement_sec(x,y,r_sec,C,n)
+function [x,y] = mouvement_sec(x,y,r_sec,C,n) %retourne les coordonées de mouvement d'un agent 
     for i=1:n
         xc = x(i);
         yc = y(i);
